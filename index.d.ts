099b22cc131f588cd08ae46baeb6e389f98248ab
akcyp <pycka.bartosz@gmail.com>
2020-07-11 21:13:39 +0200

    Initialize

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+function createLasso (canvas: HTMLCanvasElement, onChange: (polygon: string) => void | boolean): void;
+export = createLasso;
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Create Canvas Config
+ * @param {HTMLCanvasElement} canvas
+ * @param {(polygon: string) => void | boolean} onChange
+ */
+function createLasso (canvas, onChange) {
+
+}
+
+module.exports = createLasso;
